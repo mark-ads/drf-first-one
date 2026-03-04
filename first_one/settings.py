e053874882c5f8d32113049dbb1a2bf81b32bca7
mark-ads <mark.ads@list.ru>
2026-03-04 22:35:36 +0700

    Изменил БД на Постгрес.

--- first_one/settings.py
+++ first_one/settings.py
@@ -72,13 +72,23 @@ WSGI_APPLICATION = 'first_one.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "union_db",
+        "USER": "mark",
+        "PASSWORD": "teabrain",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
