3a5d6185db7b4723f929deb576fd2b8ae29bb074
mark-ads <mark.ads@list.ru>
2026-03-08 01:19:37 +0700

    Добавлена переодическая публикация мероприятий и заглушка отправки уведомлений.

--- first_one/celery.py
+++ first_one/celery.py
@@ -15,5 +15,9 @@ app.conf.beat_schedule = {
     "weather_forecast": {
         "task": "first_one.first_app.tasks.update_weather_task",
         "schedule": timedelta(minutes=5),
-    }
+    },
+    "status_check": {
+        "task": "first_one.first_app.tasks.check_event_status",
+        "schedule": timedelta(minutes=1),
+    },
 }
--- first_one/first_app/tasks.py
+++ first_one/first_app/tasks.py
@@ -1,7 +1,9 @@
-from celery import shared_task
-import requests
+from datetime import datetime
 
-from first_one.first_app.models import Event, WeatherForecast
+import requests
+from celery import shared_task
+
+from first_one.first_app.models import Event, EventNotification, WeatherForecast
 
 WEATHER_URL = "https://api.open-meteo.com/v1/forecast"
 
@@ -35,7 +37,7 @@ def update_weather_task():
             "timezone": "Asia/Bangkok",
         }
 
-        print(f'Старт получения прогноза для {event.name}')
+        print(f"Старт получения прогноза для {event.name}")
         response = requests.get(WEATHER_URL, params=params, timeout=10)
 
         response.raise_for_status()
@@ -73,4 +75,34 @@ def update_weather_task():
             wind_speed=result["wind_speed"],
         )
 
+
 update_weather_task.delay()  # type: ignore
+
+
+@shared_task
+def send_email_notification(notification_id: int):
+    """Отправить уведомления на имейл всем получателям."""
+
+    notification = EventNotification.objects.get(id=notification_id)
+    email_list = notification.recipients
+    subject = notification.email_subject
+    text = notification.email_text
+
+    for email in email_list:
+        print(f"Отправлен email по адресу {email}, тема {subject}, текст {text}")
+
+
+@shared_task
+def check_event_status():
+    events = Event.objects.filter(status=Event.StatusChoices.DRAFT)
+
+    for event in events:
+        if event.publish_date <= datetime.now():
+            print(f'Поменялся статус ивента {event.name}')
+            event.status = Event.StatusChoices.PUBLISHED
+            event.save(update_fields=["status"])
+
+            if EventNotification.objects.filter(event=event).exists():
+                print('Отдаем задачу на отправку уведомления')
+                notification = EventNotification.objects.get(event=event)
+                send_email_notification.delay(notification.id)  # type: ignore
